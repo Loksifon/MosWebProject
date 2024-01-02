4cb3c9f50f9a202fd80feb79039a7cab671bf4f8
Loksifon <LoksifonD@yandex.ru>
2024-01-02 17:13:34 +0300

    Create component UserTable

---
+++ src/components/UserTable/UserTable.tsx
@@ -0,0 +1,70 @@
+import { Space, Table, Tag } from 'antd';
+import type { ColumnsType } from 'antd/es/table';
+
+interface IDataType {
+  key: string;
+  name: string;
+  age: number;
+  address: string;
+  tags: string[];
+}
+
+type Props = {
+	userData: IDataType[]
+}
+
+export const UserTable = (props: Props) => {
+	const columns: ColumnsType<IDataType> = [
+		{
+			title: 'Name',
+			dataIndex: 'name',
+			key: 'name',
+			render: (text) => <a>{text}</a>,
+		},
+		{
+			title: 'Age',
+			dataIndex: 'age',
+			key: 'age',
+		},
+		{
+			title: 'Address',
+			dataIndex: 'address',
+			key: 'address',
+		},
+		{
+			title: 'Tags',
+			key: 'tags',
+			dataIndex: 'tags',
+			render: (_, { tags }) => (
+				<>
+					{tags.map((tag) => {
+						let color = tag.length > 5 ? 'geekblue' : 'green';
+						if (tag === 'loser') {
+							color = 'volcano';
+						}
+						return (
+							<Tag color={color} key={tag}>
+								{tag.toUpperCase()}
+							</Tag>
+						);
+					})}
+				</>
+			),
+		},
+		{
+			title: 'Action',
+			key: 'action',
+			render: (_, record) => (
+				<Space size="middle">
+					<a>Invite {record.name}</a>
+					<a>Delete</a>
+				</Space>
+			),
+		},
+	];
+	
+	
+	return (
+		<Table columns={columns} dataSource={props.userData} />
+	)
+}
