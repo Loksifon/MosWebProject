a8043b561714b7238fe70869741d849ee97b46be
Loksifon <loksifonD@yandex.ru>
2024-05-14 15:59:13 +0300

    fix config

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,5 +4,3 @@ export const SCHOOL_ROUTE = '/school-table'
 export const USER_ROUTE = '/user-table'
 export const PROFILE_ROUTE = '/profile'
 export const FORM_ROUTE ='/form'
-export const DOWNLOAD_ROUTE = '/download';
-export const CARDS_ROUTE = '/cards';
